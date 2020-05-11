3456b7557195aa4af514c970eaf8a0d541e3a7dc
yukidly <1298811823@qq.com>
2020-05-11 16:50:36 +0800

    function kaptcha -> commit

---
+++ src/main/java/com/mycode/community/KaptchaConfig.java
@@ -0,0 +1,40 @@
+package com.mycode.community;
+
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * 验证码配置类，注入spring容器
+ * @Configuration 表示这是一个配置类
+ */
+@Configuration
+public class KaptchaConfig {
+
+    //声明bean，服务启动时会被spring自动注入spring容器中
+    @Bean
+    public Producer KaptchaProducer () {
+
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width", "100");
+        properties.setProperty("kaptcha.image.height", "40");
+        properties.setProperty("kaptcha.textproducer.font.size", "32");
+        properties.setProperty("kaptcha.textproducer.font.color", "pink");
+        properties.setProperty("kaptcha.textproducer.char.string", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        //验证码干扰线-无干扰
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
+
+        DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
+        Config config = new Config(properties);
+        defaultKaptcha.setConfig(config);
+        return defaultKaptcha;
+
+    }
+
+}
